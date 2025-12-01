c02e83bdb7893a5135d8594bc9e4e3cfe7c4e8ba
asmitha399 <poojaryasmitha64@gmail.com>
2025-12-01 21:39:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import"./App.css";
+function App(){
+  const[weight,setWeight]=useState();
+  const[height,setHeight]=useState();
+  const[bmi,setBmi]=useState(null);
+  const calculateBMI=()=>{
+    if(weight&&height){
+      const heightMeter=height/100;
+      const bmiValue=(weight/(heightMeter**2)).toFixed(2);
+      setBmi(bmiValue);
+    };
+  };
+  return(
+    <div className="App"> 
+      <h1>BMI CALCULATER</h1>
+      <input
+      type="number"
+      placeholder="Weight(kg)"
+      value={weight}
+      onchange={e=>setWeight(e.target.value)} 
+      />
+      <input
+      type="number"
+      placeholder="Height(cm)"
+      value={height}
+      onchange={e=>setHeight(e.target.value)}
+ 
+      />
+      <button onClick={calculateBMI}>BMIcalculater</button>
+      {bmi&&<p>this is bmi:{bmi}</p>}
+    </div>
+  );
+};
+export default App;
